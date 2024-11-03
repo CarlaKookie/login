265bebfadc5f87b4bf0146a24467a5d579dbc9ce
CarlaKookie <car.moralesl@duocuc.cl>
2024-11-03 18:36:02 -0300

    Update auth.service.ts

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -3,23 +3,30 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-
 export class AuthService {
 
-  
   private isAuthenticated = false; // Simula si el usuario está autenticado
+
+  // Almacenar credenciales 
+  private validCredentials: { [key: string]: string } = {
+    'carla': '1234',
+    'ignacio': '1234'
+  };
+
   login(username: string, password: string): boolean {
-  // Simulamos una validación de login
-  if (username === 'carla' && password === '1234') {
-  this.isAuthenticated = true;
-  return true;
+    // Validar las credenciales
+    if (this.validCredentials[username] && this.validCredentials[username] === password) {
+      this.isAuthenticated = true;
+      return true;
+    }
+    return false;
   }
-  return false;
-  }
+
   isLoggedIn(): boolean {
-  return this.isAuthenticated;
+    return this.isAuthenticated;
   }
+
   logout() {
-  this.isAuthenticated = false;
+    this.isAuthenticated = false;
   }
-  }
+}
